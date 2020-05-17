2a671d96ea324957fb0c848e2cc800820db6a5b5
Kyle Tupling <kyletupling@hotmail.com>
2020-05-17 13:37:31 +0100

    added class fields

--- advocateui/lua/components/advocatecomponent.lua
+++ advocateui/lua/components/advocatecomponent.lua
@@ -1,7 +1,25 @@
 -- AdvocateComponent file
 AdvocateComponent = {}
-AdvocateComponent.new = function(_posX, _posY, _width, _height, _bgColor)
+AdvocateComponent.new = function(_posX, _posY)
     local self = {}
+
+    -- The targetted position of the component
+    self.PosX = _posX
+    self.PosY = _posY
+
+    -- The actual current position of the component
+    self.CurrentPosX = self.PosX
+    self.CurrentPosY = self.PosY
+
+    -- Alternative positions for toggling
+    self.AltPosX = nil
+    self.AltPosY = nil 
+
+    -- Keeps track of the state of the component
+    self.IsDisplayed = true 
+
+    -- Stores any child components
+    self.Children = {}
 
     return self
 end

138b2a98f95f815407961a3aebfca2def90e3ca6
Kyle Tupling <kyletupling@hotmail.com>
2020-05-17 20:36:23 +0100

    added SetAltPos error handle

--- advocateui/lua/components/advocatecomponent.lua
+++ advocateui/lua/components/advocatecomponent.lua
@@ -39,6 +39,8 @@ end
 -- @param _x number: The alternative position on the screen's x-axis
 -- @param _y number: The alternative position on the screen's y-axis
 function AdvocateComponent:SetAltPos(_x, _y)
+    assert(type(_x) == "number", _x .. " is not a number")
+    assert(type(_y) == "number", _y .. " is not a number")
     self.AltPosX = _x
     self.AltPosY = _y
 end

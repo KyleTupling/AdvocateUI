379becd61a98c29aa9b3c0a800ec1c62c2e2c88d
Kyle Tupling <kyletupling@hotmail.com>
2020-05-17 13:48:01 +0100

    updated to metatable OOP

--- advocateui/lua/components/advocatecomponent.lua
+++ advocateui/lua/components/advocatecomponent.lua
@@ -1,25 +1,19 @@
 -- AdvocateComponent file
 AdvocateComponent = {}
-AdvocateComponent.new = function(_posX, _posY)
-    local self = {}
+AdvocateComponent.__index = AdvocateComponent
 
-    -- The targetted position of the component
-    self.PosX = _posX
-    self.PosY = _posY
+function AdvocateComponent:new(_posX, _posY)
+    local self = {
+        PosX = _posX || 0,
+        PosY = _posY || 0,
+        CurrentPosX = _posX || 0,
+        CurrentPosX = _posY || 0,
+        AltPosX = nil,
+        AltPosY = nil,
+        IsDisplayed = true,
+        Children = {}
+    }
 
-    -- The actual current position of the component
-    self.CurrentPosX = self.PosX
-    self.CurrentPosY = self.PosY
-
-    -- Alternative positions for toggling
-    self.AltPosX = nil
-    self.AltPosY = nil 
-
-    -- Keeps track of the state of the component
-    self.IsDisplayed = true 
-
-    -- Stores any child components
-    self.Children = {}
-
+    setmetatable(self, AdvocateComponent)
     return self
 end

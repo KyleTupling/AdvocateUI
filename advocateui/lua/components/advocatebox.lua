ccb9032718bdcd8cb6dbd11a67f718c4a0cf4053
Kyle Tupling <kyletupling@hotmail.com>
2020-05-19 20:09:57 +0100

    added constructor

--- advocateui/lua/components/advocatebox.lua
+++ advocateui/lua/components/advocatebox.lua
@@ -1,1 +1,26 @@
 -- AdvocateBox file
+AdvocateBox = {}
+AdvocateBox.__index = AdvocateBox
+
+function AdvocateBox.new(_posX, _posY, _width, _height, _bgColor)
+    local self = {}
+
+    self.Parent = nil
+
+    self.PosX = _posX || 0
+    self.PosY = _posY || 0
+
+    self.CurrentPosX = self.PosX
+    self.CurrentPosY = self.PosY
+
+    self.AltPosX = nil 
+    self.AltPosY = nil
+
+    self.IsDisplayed = true 
+
+    self.Children = {}
+
+    setmetatable(self, AdvocateBox)
+    return self
+end
+

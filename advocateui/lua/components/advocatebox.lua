4e01150c9f4e88c37045ccdddb83b040f4d7b9ee
Kyle Tupling <kyletupling@hotmail.com>
2020-05-19 20:16:27 +0100

    added draw method

--- advocateui/lua/components/advocatebox.lua
+++ advocateui/lua/components/advocatebox.lua
@@ -10,6 +10,11 @@ function AdvocateBox.new(_posX, _posY, _width, _height, _bgColor)
     self.PosX = _posX || 0
     self.PosY = _posY || 0
 
+    self.Width = _width
+    self.Height = _height
+
+    self.BgColor = _bgColor
+
     self.CurrentPosX = self.PosX
     self.CurrentPosY = self.PosY
 
@@ -24,3 +29,19 @@ function AdvocateBox.new(_posX, _posY, _width, _height, _bgColor)
     return self
 end
 
+function AdvocateBox:Draw()
+    if(self.IsDisplayed) then
+        self.CurrentPosX = Lerp(10 * FrameTime(), self.CurrentPosX, self.PosX)
+        self.CurrentPosY = Lerp(10 * FrameTime(), self.CurrentPosY, self.PosY)
+    else
+        if(self.AltPosX) then self.CurrentPosX = Lerp(10 * FrameTime(), self.CurrentPosX, self.AltPosX) end
+        if(self.AltPosY) then self.CurrentPosY = Lerp(10 * FrameTime(), self.CurrentPosY, self.AltPosY) end
+    end
+
+    -- Base
+    draw.RoundedBox(0, self.CurrentPosX, self.CurrentPosY, self.Width, self.Height, self.BgColor)
+
+    for i = 1, #self.Children do
+        self.Children[i].Draw()
+    end
+end
